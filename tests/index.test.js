fd006179bce20da0235bbf5e92f49d1c209139b1
CaritoDasilva <caritodasilva@gmail.com>
2018-09-01 00:07:35 -0300

    se sube el readme

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,5 @@
 const mdLinks = require('./../src/js/index');
 
-test('espera que se una función', () => {
-  expect(typeof mdLinks).toBe('function');
+test('espera que sea una función', () => {
+  expect(typeof mdLinks).toBe('function' || 'object');
 });
